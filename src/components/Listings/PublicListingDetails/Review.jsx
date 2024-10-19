71a5711651441db861e366431dc9d4694632f377
Deustech-Labs <aaku17.10.1998@gmail.com>
2024-10-19 21:37:00 +0530

    added comment

--- src/components/Listings/PublicListingDetails/Review.jsx
+++ src/components/Listings/PublicListingDetails/Review.jsx
@@ -81,7 +81,7 @@ export const Review = () => {
                 </div>
               </li>
             ))}
-
+            {/* add review comp */}
             <AddReview getListingReview={getListingReview} user={user} />
           </ul>
         </div>
